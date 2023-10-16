25cba1ecadef64d40e22f497a89f87e3acfe3944
olisah mbaeyi <olisah0882@gmail.com>
2023-10-16 16:23:54 -0400

    new commit for maven

---
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+		string name = "mike";
+		System.out.println(name);
+		Integer age ="25";
+		System.out.println(age +5);
+
+	}
+
+}
